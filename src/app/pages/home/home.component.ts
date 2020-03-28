046b8479a32d45b332ebd71813c1cb5cd747f4b7
JohnHu <john.hu@carestream.com>
2020-03-28 19:50:00 +0800

    Finish banner

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectionStrategy } from '@angular/core';
+import { HomeService } from 'src/app/services/home.service';
+import { Banner } from 'src/app/services/data-types/common.types';
+import { NzCarouselComponent } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.less']
+  styleUrls: ['./home.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
+  carouselActiveIndex = 0;
+  banners: Banner[];
 
-  constructor() { }
+  @ViewChild(NzCarouselComponent, {static: true}) private nzCarousel: NzCarouselComponent;
+
+  constructor(private homeService: HomeService) {
+    this.homeService.getBanners().subscribe(banners=>{
+      this.banners = banners;
+    });
+  }
 
   ngOnInit() {
   }
+
+  onBeforeChange({ to }) {
+    console.log(to);
+    this.carouselActiveIndex = to;
+  }
+
+  onChangeSlide(type: 'pre' | 'next') {
+    this.nzCarousel[type]();
+  }
+
 
 }
